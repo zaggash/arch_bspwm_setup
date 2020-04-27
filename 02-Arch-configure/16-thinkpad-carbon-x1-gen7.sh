4625d6bba89d3850c3c3608ac732f03f6c9a5618
Alexandre Pinon <1724149+zaggash@users.noreply.github.com>
2020-04-27 15:52:51 +0200

    Edit modprobe alsa - set Internal card to 0

--- 02-Arch-configure/16-thinkpad-carbon-x1-gen7.sh
+++ 02-Arch-configure/16-thinkpad-carbon-x1-gen7.sh
@@ -5,14 +5,15 @@ echo "## Install Thinkpad Carbon Carbon X1 Gen7 specific packages ##"
 ## Lenovo throttled fix
 sudo pacman -Syyu --noconfirm --needed \
 	throttled \
-	acpi_call \
+	acpi_call \ # Used by tlp on thinkpad
 	sof-firmware
 	
 echo "## Setup X1C7 sound/microphone ##"
 echo "\
+options snd slots=snd_soc_skl_hda_dsp
 blacklist snd_hda_intel
 blacklist snd_soc_skl " \
-	> /etc/modprobe.d/blacklist.conf
+	> /etc/modprobe.d/alsa.conf
 
 echo "## Setup Touchpad config ##"
 sudo install -Dm 644 system/X11/etc/X11/xorg.conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/
