8c5ed597ab53f099cb553706541ca153acab56b0
Alexandre Pinon <1724149+zaggash@users.noreply.github.com>
2020-05-11 17:20:53 +0200

    Update 16-thinkpad-carbon-x1-gen7.sh

--- 02-Arch-configure/16-thinkpad-carbon-x1-gen7.sh
+++ 02-Arch-configure/16-thinkpad-carbon-x1-gen7.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 set -e
 
+## https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_7)
+
 echo "## Install Thinkpad Carbon Carbon X1 Gen7 specific packages ##"
 ## Lenovo throttled fix
 sudo pacman -Syyu --noconfirm --needed \
+	# Lenovo CPU throttle fix
 	throttled \
-	acpi_call \ # Used by tlp on thinkpad
+	# Used by tlp on thinkpad
+	acpi_call \
 	sof-firmware
-	
-#echo "## Setup X1C7 sound/microphone ##"
-#echo "\
-#options snd slots=snd_soc_skl_hda_dsp
-#blacklist snd_hda_intel
-#blacklist snd_soc_skl " \
-#	> /etc/modprobe.d/alsa.conf
 
 echo "## Setup Touchpad config ##"
 sudo install -Dm 644 system/X11/etc/X11/xorg.conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/
